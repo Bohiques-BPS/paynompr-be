5fd4fb2f7e71376d75e3394f6d7b4c166203d1e7
zeuspod <josseluism@gmail.com>
2024-07-11 08:08:00 -0400

    API:
    Se mnodifico el seed de period para que no se carguen varias veces

--- routers/period.py
+++ routers/period.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
-from controllers.period import create_weekly_periods, create_biweekly_periods, create_monthly_periods
+from controllers.period import create_weekly_periods, create_biweekly_periods, create_monthly_periods, get_all_periods_controller, get_periods_by_year_and_type_controller
+from models.periods import PeriodType
 from schemas.period import PeriodCreate, PeriodRead
 
 period_routes = APIRouter()
@@ -16,3 +17,15 @@ def create_biweekly_periods_route(year: int):
 @period_routes.post("/monthly/", response_model=list[PeriodRead])
 def create_monthly_periods_route(year: int):
     return create_monthly_periods(year)
+
+### get all periods 
+@period_routes.get("/periods", response_model=list[PeriodRead])
+def get_all_period():
+    return get_all_periods_controller()
+
+
+@period_routes.get("/periods_by_year_and_type",response_model=list[PeriodRead])
+def get_periods_by_year_and_type(year: int, period_type: PeriodType):
+    return get_periods_by_year_and_type_controller(year, period_type)
+
+
