42c868a2e34e945855d4675841a299b49bfe5baa
driver001 <andres.udo.info@gmail.com>
2024-07-15 10:19:42 -0400

    cambios para el formato de diseño

--- controllers/companies_controller.py
+++ controllers/companies_controller.py
@@ -102,8 +102,7 @@ def get_all_companies_controller(user):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An error occurred: {str(e)}"
         )
-    finally:
-        session.close()
+   
 
 def get_all_company_and_employer_controller(user, company_id, employers_id):
     try:    
--- database/seed/period.py
+++ database/seed/period.py
@@ -6,34 +6,28 @@ from models.periods import Period, PeriodType
 from controllers.period import create_weekly_periods, create_biweekly_periods, create_monthly_periods
 
 def periods_exist(year: int, period_type: PeriodType) -> bool:
-    return session.query(Period).filter_by(year=year, period_type=period_type).first() is not None
+    return session.query(Period).filter(Period.year==year, Period.period_type==period_type).first() is not None
 
 def initialize_periods():
-    try:
-        # Create periods for the current year and the previous year
-        current_year = date.today().year
-        previous_year = current_year - 1
+    
+    # Create periods for the current year and the previous year
+    current_year = date.today().year
+    previous_year = current_year - 1
 
-        if not periods_exist(previous_year, PeriodType.WEEKLY):
-            create_weekly_periods(previous_year)
-        if not periods_exist(current_year, PeriodType.WEEKLY):
-            create_weekly_periods(current_year)
-        if not periods_exist(previous_year, PeriodType.BIWEEKLY):
-            create_biweekly_periods(previous_year)
-        if not periods_exist(current_year, PeriodType.BIWEEKLY):
-            create_biweekly_periods(current_year)
-        if not periods_exist(previous_year, PeriodType.MONTHLY):
-            create_monthly_periods(previous_year)
-        if not periods_exist(current_year, PeriodType.MONTHLY):
-            create_monthly_periods(current_year)
 
-        session.commit()
-        print("Periods initialized successfully")
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"An error occurred while initializing periods: {e}",
-        )
-    finally:
-        session.close()
+    if not periods_exist(previous_year, PeriodType.WEEKLY):
+        create_weekly_periods(previous_year)
+    if not periods_exist(current_year, PeriodType.WEEKLY):
+        create_weekly_periods(current_year)
+    if not periods_exist(previous_year, PeriodType.BIWEEKLY):
+        create_biweekly_periods(previous_year)
+    if not periods_exist(current_year, PeriodType.BIWEEKLY):
+        create_biweekly_periods(current_year)
+    if not periods_exist(previous_year, PeriodType.MONTHLY):
+        create_monthly_periods(previous_year)
+    if not periods_exist(current_year, PeriodType.MONTHLY):
+        create_monthly_periods(current_year)
+
+    session.commit()
+    print("Periods initialized successfully")
+    
